ea5fbb74fff19b462f7706ea5f7e02c25cfdfee6
Serhii Khomenko <sakhom16@gmail.com>
2024-02-27 21:56:33 +0200

    add etherscan API key

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 //SEPOLIA
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 module.exports = {
   solidity: "0.8.20",
@@ -11,5 +12,11 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true
   }
 };
