ff5cda925afa071c57001396f912547f47a38ba3
Serhii Khomenko <sakhom16@gmail.com>
2024-02-27 15:53:44 +0200

    add Burnable, Ownable, Cappable; add unit tests

--- contracts/SAKToken.sol
+++ contracts/SAKToken.sol
@@ -2,14 +2,55 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract SAKToken is ERC20 {
 
-    constructor() ERC20("SAK Token", "SAK") {
-        _mint(msg.sender, 4_300_000 * 10**8);
+contract SAKToken is ERC20Burnable, Ownable, ERC20Capped {
+    
+    uint8 private DECIMALS = 8;
+    uint256 private immutable CAP = 4_300_000;
+    string private TOKEN_NAME = "SAK Token";
+    string private TOKEN_SYMBOL = "SAK";
+    mapping(address account => uint256) private _balances;
+    uint256 private _totalSupply;
+
+    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) ERC20Capped(CAP * 10**decimals()) Ownable(msg.sender){
+        
     }
 
     function decimals() public view override returns (uint8) {
-        return 8;
+        return DECIMALS;
+    }
+
+    function mint(uint256 tokenNumber) public onlyOwner {
+        _mint(owner(), tokenNumber);
+    }
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Capped)  {
+        super._update(from, to, value);
+
+        if (from == address(0)) {
+            uint256 maxSupply = cap();
+            uint256 supply = totalSupply();
+            if (supply > maxSupply) {
+                revert ERC20ExceededCap(supply, maxSupply);
+            }
+        }
+
+        if (to == address(0)) {
+            unchecked {
+                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
+                _totalSupply -= value;
+            }
+        } else {
+            unchecked {
+                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
+                _balances[to] += value;
+            }
+        }
+
+        emit Transfer(from, to, value);
     }
 }
--- test/SAKToken.js
+++ test/SAKToken.js
@@ -4,17 +4,17 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { expect } = require("chai");
 
-describe("SAKToken", function () {
+describe("SAKToken test suit", function () {
 
   async function deploySakTokenFixture() {
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, otherAccount, anotherAccount] = await ethers.getSigners();
     const SAKToken = await ethers.getContractFactory("SAKToken");
     const sakToken = await SAKToken.deploy();
-
-    return { sakToken, owner, otherAccount };
+    const sakTokenAtOtherAccount = await sakToken.connect(otherAccount);
+    return { sakToken, owner, otherAccount, anotherAccount, sakTokenAtOtherAccount };
   }
 
-  describe("SAKToken", function () {
+  describe("SAKToken base functions", function () {
     it.only("Should have correct name - SAK Token", async function () {
       const { sakToken } = await loadFixture(deploySakTokenFixture);
       expect(await sakToken.name()).to.equal("SAK Token");
@@ -30,14 +30,89 @@ describe("SAKToken", function () {
       expect(await sakToken.decimals()).to.equal(8);
     });
 
-    it.only("Should have total supply - 430 000 000 000 000", async function () {
-      const { sakToken, owner } = await loadFixture(deploySakTokenFixture);
-      expect(await sakToken.totalSupply()).to.equal(4_300_000 * 10**8);
+    it.only("Should have total supply 4_300_000 tokens", async function () {
+      const { sakToken } = await loadFixture(deploySakTokenFixture);
+      expect(await sakToken.totalSupply()).to.equal(0);
     });
     
     it.only("Owner address should have balance of all total supply", async function () {
       const { sakToken, owner } = await loadFixture(deploySakTokenFixture);
-      expect(await sakToken.balanceOf(owner)).to.equal(4_300_000 * 10**8);
+      expect(await sakToken.balanceOf(owner)).to.equal(0);
+    });
+
+  });
+
+  describe("SAKToken transfer functions", function () {
+    
+    it.only("mint() works correctly", async function () {
+      const { sakToken, owner } = await loadFixture(deploySakTokenFixture);
+      await sakToken.mint(ethers.parseUnits("300000", 8));
+      expect(await sakToken.totalSupply()).to.equal(ethers.parseUnits("300000", 8));
+      expect(await sakToken.balanceOf(owner)).to.equal(ethers.parseUnits("300000", 8));
+    });
+    
+    it.only("after transfer() completed sender and reciever addresses should have correct balance", async function () {
+      const { sakToken, owner, otherAccount } = await loadFixture(deploySakTokenFixture);
+      await sakToken.mint(ethers.parseUnits("400000", 8));
+      const tx = await sakToken.transfer(otherAccount, ethers.parseUnits("300000", 8));
+      await tx.wait();
+      expect(await sakToken.balanceOf(owner)).to.equal(ethers.parseUnits("100000", 8));
+      expect(await sakToken.balanceOf(otherAccount)).to.equal(ethers.parseUnits("300000", 8));
+    });
+
+    it.only("after approve() completed allowance() has changed correctly", async function () {
+      const { sakToken, owner, otherAccount } = await loadFixture(deploySakTokenFixture);
+      await sakToken.approve(otherAccount, ethers.parseUnits("500000", 8));
+      expect(await sakToken.allowance(owner, otherAccount)).to.equal(ethers.parseUnits("500000", 8));
+    });
+
+    it.only("after transferFrom() completed sender and reciever addresses should have correct balance", async function () {
+      const { sakToken, owner, otherAccount, anotherAccount, sakTokenAtOtherAccount } = await loadFixture(deploySakTokenFixture);
+      await sakToken.mint(ethers.parseUnits("400000", 8));
+      expect(await sakToken.balanceOf(owner)).to.equal(ethers.parseUnits("400000", 8));
+      await sakToken.approve(otherAccount, ethers.parseUnits("300000", 8));
+      expect(await sakToken.allowance(owner, otherAccount)).to.equal(ethers.parseUnits("300000", 8));
+
+      const tx = await sakTokenAtOtherAccount.transferFrom(owner, anotherAccount, ethers.parseUnits("300000", 8));
+      await tx.wait();
+
+      // console.log("owner address: " + owner.address + ", balance " + await sakToken.balanceOf(owner));
+      // console.log("otherAccount address: " + otherAccount.address + ", balance " + await sakToken.balanceOf(otherAccount));
+      // console.log("sakTokenAtOtherAccount address: " + anotherAccount.address + ", balance " + await sakToken.balanceOf(anotherAccount));
+      
+      expect(await sakToken.balanceOf(owner)).to.equal(ethers.parseUnits("100000", 8));
+      expect(await sakToken.balanceOf(anotherAccount)).to.equal(ethers.parseUnits("300000", 8));
+    });
+
+    it.only("user without allowance() can't use transferFrom() ", async function () {
+      const { sakToken, owner, otherAccount, anotherAccount, sakTokenAtOtherAccount } = await loadFixture(deploySakTokenFixture);
+      await sakToken.mint(ethers.parseUnits("400000", 8));
+      await sakToken.approve(otherAccount, ethers.parseUnits("300000", 8));
+      await expect(sakTokenAtOtherAccount.transferFrom(owner, anotherAccount, ethers.parseUnits("400000", 8))).to.be.reverted;
+    });
+  });
+  describe("SAKToken burnable functions", function () {
+    it.only("burn() works correctly", async function () {
+      const { sakToken, owner } = await loadFixture(deploySakTokenFixture);
+      await sakToken.mint(ethers.parseUnits("400000", 8));
+      await sakToken.burn(ethers.parseUnits("300000", 8));
+      expect(await sakToken.totalSupply()).to.equal(ethers.parseUnits("100000", 8));
+      expect(await sakToken.balanceOf(owner)).to.equal(ethers.parseUnits("100000", 8));
+    });
+    it.only("contract owner is correct", async function () {
+      const { sakToken, owner } = await loadFixture(deploySakTokenFixture);
+      expect(await sakToken.owner()).to.equal(owner);
+    });
+    it.only("only owner can mint", async function () {
+      const { sakTokenAtOtherAccount } = await loadFixture(deploySakTokenFixture);
+      await expect(sakTokenAtOtherAccount.mint(ethers.parseUnits("400000", 8))).to.be.reverted;
+    });
+    it.only("totalSupply can't be more than CAP value", async function () {
+      const { sakToken } = await loadFixture(deploySakTokenFixture);
+      //mint max value
+      await sakToken.mint(ethers.parseUnits("4300000", 8));
+      //add more 100 tokens
+      await expect(sakToken.mint(ethers.parseUnits("100", 8))).to.be.reverted;
     });
   });
 });
